8bc40ce22f833c1f56530fdf81e1b91fd9e7375b
Michael Loveland <cheetolov@gmail.com>
2015-05-29 15:36:01 +0000

    Fixed user js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   // Watch for a form submission:
-  $("#form-submit-btn").click(function(event){
+  $("#form-submit-btn").click(function(event) {
     event.preventDefault();
     $('input[type=submit]').prop('disabled', true);
     var error = false;
@@ -10,7 +10,7 @@ $(document).ready(function() {
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
     if (!error) {
-      // Get Stripe token:
+      // Get the Stripe token:
       Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
@@ -23,14 +23,11 @@ $(document).ready(function() {
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
-    
     // Get the token from the response:
     var token = response.id;
-    
     // Add the token to the form:
-    f.append('<input type"hidden" name="user[stripe_card_token]" value="' + token + '" />');
-    
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
     // Submit the form:
-    f.get(0).submit();
+    f.get(0).submit(); 
   }
 });
